4b0ad9a74f5fe19e150ff83b1c5c90b8941013e5
Gideon Osei Acheampong <gideonz823@gmail.com>
2025-09-28 14:03:19 -0700

    python introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# basic_operations.py
+
+# Define the numbers
+number1 = 10
+number2 = 5
+
+# Perform operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Print results
+print(fAddition of {number1} and {number2} is {sum_result})
+print(fSubtraction of {number1} and {number2} is {difference_result})
+print(fMultiplication of {number1} and {number2} is {product_result})
+
